c0e0376c79120c463134bae2caadf366eaaa4817
Alain Bonneau <alaindev97@gmail.com>
2026-01-29 01:08:19 +0100

    convert banner to tsx

---
+++ src/types/cart.d.ts
@@ -0,0 +1,11 @@
+export type CartItem = {
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    price: number;
+    light: number;
+    water: number;
+    cover: string;
+    amount: number;
+};
