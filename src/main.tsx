d9a012841c417233d9599747a5974e606f590b0c
Alain Bonneau <alaindev97@gmail.com>
2026-01-29 19:32:22 +0100

    convert app to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// Configuration TypeScript pour ignorer l'erreur de type à supprimer quand App sera en TypeScript
-// @ts-expect-error
 import App from "./components/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
